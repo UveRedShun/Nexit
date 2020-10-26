791011d9131b7c73617e598ec6e3f25b59eebe13
Shunta Watabe <s.ubel.w.w.09@gmail.com>
2020-10-27 02:02:18 +0900

    sliderの作成

--- assets/js/style.js
+++ assets/js/style.js
@@ -7,3 +7,57 @@ document.querySelector('.navbar-toggler').onclick = function(){
     document.querySelector('#navbar').classList.add('show');
     }
 }
+
+$(function(){
+    $(window).scroll(function (){
+        $('.about-image').each(function(){
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 100){
+                $(this).addClass('fadein');
+            }
+        });
+        $('.members-img').each(function(){
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 100){
+                $(this).addClass('fadein');
+            }
+        });
+        $('.services-imgBox').each(function(){
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 100){
+                $(this).addClass('fadein');
+            }
+        });
+        $('.services-imgBox__Seo').each(function(){
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 100){
+                $(this).addClass('fadein');
+            }
+        });
+    });    
+});
+
+var swiper = new Swiper('.swiper-container', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
