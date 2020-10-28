fae12c534014b937d5125065623187a2bffd8315
sho <sho19980131@gmail.com>
2020-10-29 00:19:14 +0900

    smooth scroll Implementation

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,12 +1,13 @@
-//ハンバーガーメニューをクリックしたとき
-document.querySelector('.navbar-toggler').onclick = function(){
-    //開いた時と閉じたとき
-    if(document.getElementById('navbar').classList.contains('show')){
-        document.querySelector('#navbar').classList.remove('show');
-    } else {
-    document.querySelector('#navbar').classList.add('show');
-    }
-}
+$(function(){
+    $('a[href^="#"]').click(function(){
+        var speed = 1000;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+    });
+});
 
 $(function(){
     $(window).scroll(function (){
