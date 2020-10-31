cb1b672fed14226ffa5772424095c4b1bcbe6921
Shunta Watabe <s.ubel.w.w.09@gmail.com>
2020-10-31 17:55:06 +0900

    スクロールjs調節

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,9 +1,10 @@
 $(function(){
     $('a[href^="#"]').click(function(){
         var speed = 1000;
+        var mHeight = 50;
         var href= $(this).attr("href");
         var target = $(href == "#" || href == "" ? 'html' : href);
-        var position = target.offset().top;
+        var position = target.offset().top + mHeight;
         $("html, body").animate({scrollTop:position}, speed, "swing");
         return false;
     });
